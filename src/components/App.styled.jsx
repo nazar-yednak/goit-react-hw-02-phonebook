a3cce2ecc36663df77322ea1e71f883d5485529f
nazar-yednak <93836409+nazar-yednak@users.noreply.github.com>
2022-08-03 23:49:04 +0300

    add

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  background-color: wheat;
+  width: 300px;
+  padding: 25px;
+`;
